c55ddb25083e35aa7eb3dde880885d7251e2eb31
iranga <irangacomz@gmail.com>
2026-05-13 13:56:07 +1000

    package the emotiondetector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -35,3 +35,18 @@ def emotion_detector(text_to_analyze):
         "sadness": sadness,
         "dominant_emotion": dominant_emotion
     }
+
+def emotion_detector1(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+
+    payload = {
+        "raw_document": {"text": text_to_analyze}
+    }
+
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    return response.text
